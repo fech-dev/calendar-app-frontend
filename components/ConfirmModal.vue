df2a1c418f571fdaa0b7c6a6339926ad1435554d
Federico Mameli <federico_mameli@yahoo.com>
2024-04-14 22:36:26 +0000

    feat: create ConfirmModal

---
+++ components/ConfirmModal.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+interface Props {
+  loading?: boolean;
+  message: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  loading: false,
+});
+
+interface Emits {
+  (e: "confirm"): void;
+  (e: "cancel"): void;
+}
+
+const emit = defineEmits<Emits>();
+</script>
+
+<template>
+  <UModal>
+    <UCard :ui="{ footer: { base: 'flex justify-end gap-2' } }">
+      <p class="text-red-500 text-lg">{{ message }}</p>
+
+      <template #footer>
+        <UButton
+          color="red"
+          variant="soft"
+          label="Cancel"
+          @click="emit('cancel')"
+        />
+
+        <UButton
+          :loading
+          color="red"
+          label="Confirm"
+          @click="emit('confirm')"
+        />
+      </template>
+    </UCard>
+  </UModal>
+</template>
